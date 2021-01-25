01dd95dd85457a15e01bba22404a400bf8eefa65
Jamie Wilkinson <jamie@jamiedubs.com>
2021-01-25 09:05:30 -0500

    Only update ETL if there have been new commits

--- etl-update-and-restart.sh
+++ etl-update-and-restart.sh
@@ -2,7 +2,19 @@
 set -e
 
 cd /root/blockchain-etl # for cronjob
+latest=$(cat LATEST_BUILD)
+echo "LATEST_BUILD: $latest"
+echo "Existing HEAD: $(git rev-parse HEAD)"
 git pull -q origin master 
-make release
-make migrations
-make start
+head=$(git rev-parse HEAD)
+echo "Post-pull HEAD: $head"
+
+if [ "$head" != "$latest" ]; then
+  echo "New commits, rebuilding and restarting..."
+  make release
+  git rev-parse HEAD > LATEST_BUILD
+  make migrations
+  make start
+else
+  echo "Commit hashes are the same, not doing anything"
+fi
